082b01fd3585e041790fa9b066b5c0e52e4812a8
Vivek Kumar Shukla <vkshukla9918@gmail.com>
2022-09-14 15:20:05 +0530

    data pre processing, subtracting average of each column .

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -9,4 +9,10 @@ data.at[0,'U_avg'] = data['U'].mean()
 data.at[0,'V_avg'] = data['V'].mean() # using mean() taking average of U,V and W and making column in csv file
 data.at[0,'W_avg'] = data['W'].mean()
 
+data['U-U_avg'] = data['U'] - data['U_avg'][0]
+data['V-V_avg'] = data['V'] - data['V_avg'][0]  
+data['W-W_avg'] = data['W'] - data['W_avg'][0]    
+
+#data pre processing, subtracting avg of each column to each element of that column and 
+# creating new column for pre processed data
 data.to_csv('octant_output1.csv')
