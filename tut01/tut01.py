3cb3ece842294343f45ef4ad79a337757ba1c209
Vivek Kumar Shukla <vkshukla9918@gmail.com>
2022-09-14 15:09:09 +0530

    average of u, v, and w columns

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -5,5 +5,8 @@
 import pandas as pd                     #imported pandas library
 data = pd.read_csv('octant_input.csv')  #reading 'octant_input.csv' file 
 
+data.at[0,'U_avg'] = data['U'].mean()
+data.at[0,'V_avg'] = data['V'].mean() # using mean() taking average of U,V and W and making column in csv file
+data.at[0,'W_avg'] = data['W'].mean()
 
-
+data.to_csv('octant_output1.csv')
