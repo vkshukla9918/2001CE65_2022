804dc0743a1fbcfd91c19664f1223b5736b677eb
Vivek Kumar Shukla <vkshukla9918@gmail.com>
2022-09-30 02:29:27 +0530

    octant identification and creating a new column  octact

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -8,3 +8,29 @@ data.at[0,'W Avg'] = data['W'].mean()
 data["U'=U - U avg"] = data['U'] - data['U Avg'][0]
 data["V'=V - V avg"] = data['V'] - data['V Avg'][0]  #data pre processing
 data["W'=W - W avg"] = data['W'] - data['W Avg'][0]
+
+octact = [] #creating new list
+
+for i in range(len(data)):
+    x = data["U'=U - U avg"][i]
+    y = data["V'=V - V avg"][i]     #created new variable
+    z = data["W'=W - W avg"][i]
+    
+    if x > 0 and y > 0 and z > 0:
+        octact.append(1)
+    elif x > 0 and y > 0 and z < 0:
+        octact.append(-1)                 
+    elif x < 0 and y > 0 and z > 0:       
+        octact.append(2)                  
+    elif x < 0 and y > 0 and z < 0:       # Octant identification and appending in a list
+        octact.append(-2)                 
+    elif x < 0 and y < 0 and z > 0:       
+        octact.append(3)                  
+    elif x < 0 and y < 0 and z < 0:       
+        octact.append(-3)                 
+    elif x > 0 and y < 0 and z > 0:
+        octact.append(4)
+    elif x > 0 and y < 0 and z < 0:
+        octact.append(-4)
+data['Octact'] = octact.copy()  
+
