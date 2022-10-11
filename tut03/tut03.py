bcfd2fb43499c3bd9ff66e022f1abd4069d3e477
Vivek Kumar Shukla <vkshukla9918@gmail.com>
2022-10-11 20:45:42 +0530

    output file

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -67,7 +67,7 @@ for i in range(x-1):  #appending subsequences of a octant
             a = 0
     if(octact[i] == -1 and octact[i+1] == -1):
         b += 1
-    elif((octact[i-1]!= -1 and octact[i] == -1 and octact[i+1] != -1) or (i== 0 and octact[0] == -1 and octact[1]!= -1)or (octact[x-2] != -1 and octact[x-1] == -1)):
+    elif((octact[i-1]!= -1 and octact[i] == -1 and octact[i+1] != -1) or (octact[0] == -1 and octact[1]!= -1)or (octact[x-2] != -1 and octact[x-1] == -1)):
         max_1.append(1)
     else:
         if b>0:
@@ -75,7 +75,7 @@ for i in range(x-1):  #appending subsequences of a octant
             b = 0
     if(octact[i] == 2 and octact[i+1] == 2):
         c += 1
-    elif((octact[i-1]!= 2 and octact[i] == 2 and octact[i+1] != 2) or (i== 0 and octact[0] == 2 and octact[1]!= 2)or (octact[x-2] != 2 and octact[x-1] == 2)):
+    elif((octact[i-1]!= 2 and octact[i] == 2 and octact[i+1] != 2) or (octact[0] == 2 and octact[1]!= 2)or (octact[x-2] != 2 and octact[x-1] == 2)):
         max2.append(1)
     else:
         if c>0:
@@ -83,7 +83,7 @@ for i in range(x-1):  #appending subsequences of a octant
             c = 0
     if(octact[i] == -2 and octact[i+1] == -2):
         d += 1
-    elif((octact[i-1]!= -2 and octact[i] == -2 and octact[i+1] != -2) or (i== 0 and octact[0] == -2 and octact[1]!= -2)or (octact[x-2] != -2 and octact[x-1] == -2)):
+    elif((octact[i-1]!= -2 and octact[i] == -2 and octact[i+1] != -2) or (octact[0] == -2 and octact[1]!= -2)or (octact[x-2] != -2 and octact[x-1] == -2)):
         max_2.append(1)
     else:
         if d>0:
@@ -91,7 +91,7 @@ for i in range(x-1):  #appending subsequences of a octant
             d = 0
     if(octact[i] == 3 and octact[i+1] == 3):
         e += 1
-    elif((octact[i-1]!= 3 and octact[i] == 3 and octact[i+1] != 3) or (i== 0 and octact[0] == 3 and octact[1]!= 3)or (octact[x-2] != 3 and octact[x-1] == 3)):
+    elif((octact[i-1]!= 3 and octact[i] == 3 and octact[i+1] != 3) or (octact[0] == 3 and octact[1]!= 3)or (octact[x-2] != 3 and octact[x-1] == 3)):
         max3.append(1)
     else:
         if e>0:
@@ -99,7 +99,7 @@ for i in range(x-1):  #appending subsequences of a octant
             e = 0
     if(octact[i] == -3 and octact[i+1] == -3):
         f += 1
-    elif((octact[i-1]!= -3 and octact[i] == -3 and octact[i+1] != -3) or (i== 0 and octact[0] == -3 and octact[1]!= -3)or (octact[x-2] != -3 and octact[x-1] == -3)):
+    elif((octact[i-1]!= -3 and octact[i] == -3 and octact[i+1] != -3) or (octact[0] == -3 and octact[1]!= -3)or (octact[x-2] != -3 and octact[x-1] == -3)):
         max_3.append(1)
     else:
         if f>0:
@@ -107,7 +107,7 @@ for i in range(x-1):  #appending subsequences of a octant
             f = 0
     if(octact[i] == 4 and octact[i+1] == 4):
         g += 1
-    elif((octact[i-1]!= 4 and octact[i] == 4 and octact[i+1] != 4) or (i== 0 and octact[0] == 4 and octact[1]!= 4)or (octact[x-2] != 4 and octact[x-1] == 4)):
+    elif((octact[i-1]!= 4 and octact[i] == 4 and octact[i+1] != 4) or (octact[0] == 4 and octact[1]!= 4)or (octact[x-2] != 4 and octact[x-1] == 4)):
         max4.append(1)
     else:
         if g>0:
@@ -115,7 +115,7 @@ for i in range(x-1):  #appending subsequences of a octant
             g = 0
     if(octact[i] == -4 and octact[i+1] == -4):
         h += 1
-    elif((octact[i-1]!= -4 and octact[i] == -4 and octact[i+1] != -4) or (i== 0 and octact[0] == -4 and octact[1]!= -4)or (octact[x-2] != -4 and octact[x-1] == -4)):
+    elif((octact[i-1]!= -4 and octact[i] == -4 and octact[i+1] != -4) or (octact[0] == -4 and octact[1]!= -4)or (octact[x-2] != -4 and octact[x-1] == -4)):
         max_4.append(1)
     else:
         if h>0:
