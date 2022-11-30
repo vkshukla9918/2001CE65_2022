5b8e023954ca4fcdf70f99462f4ab90dede7c193
Vivek Kumar Shukla <vkshukla9918@gmail.com>
2022-11-30 17:05:48 +0530

    imported libraries and taking input on web page

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -1,28 +1,24 @@
-
+import time
 from datetime import datetime
 start_time = datetime.now()
-
-#Help
-def proj_octant_gui():
-	pass
-
-
-###Code
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+import streamlit as st
+import numpy as np
+from numpy import nan
+import pandas as pd
+from pathlib import Path
+import math 
+import openpyxl                          #imported library
+from openpyxl import workbook
+from openpyxl.styles import Border,Side
+from openpyxl.styles import PatternFill
 
 
-proj_octant_gui()
-
-
-
-
+try:
+	st.title('Welcome to Octant Batch Processing')
+	uploaded_file = st.file_uploader("Choose a file",type=["xlsx"], accept_multiple_files=True)
+	Mod = int(st.number_input("Enter the mod value:"))
+except:
+	print('strealit is not imported')
 
 
 #This shall be the last lines of the code.
